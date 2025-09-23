f23ed6f7cb64c6204e11e7830d64c8700f941031
yordanos habtamu <jordrichh7@gmail.com>
2025-09-23 22:55:06 +0300

    some updates on main.go

--- cmd/main.go
+++ cmd/main.go
@@ -3,38 +3,40 @@ package main
 import (
 	"database/sql"
 	"log"
+	"os"
 
 	"github.com/go-sql-driver/mysql"
 	"github.com/yordanos-habtamu/EcomGo.git/cmd/api"
-	"github.com/yordanos-habtamu/EcomGo.git/config"
 	"github.com/yordanos-habtamu/EcomGo.git/db"
 )
 
 func main() {
-    db,err := db.NewMysqlStorage(mysql.Config{
-		User:config.Envs.DB_USER,
-		Passwd: config.Envs.DB_PWD,
-		Addr: config.Envs.DB_ADDR,
-		DBName: config.Envs.DB_NAME,
-		Net: "tcp",
+	// Read Railway env vars directly
+	cfg := mysql.Config{
+		User:                 os.Getenv("MYSQLUSER"),
+		Passwd:               os.Getenv("MYSQLPASSWORD"),
+		Addr:                 os.Getenv("MYSQLHOST") + ":" + os.Getenv("MYSQLPORT"),
+		DBName:               os.Getenv("MYSQLDATABASE"),
+		Net:                  "tcp",
 		AllowNativePasswords: true,
-		ParseTime: true,
-	})
-	if err != nil{
+		ParseTime:            true,
+	}
+
+	database, err := db.NewMysqlStorage(cfg)
+	if err != nil {
 		log.Fatal(err)
 	}
-    
-	initStorage(db)
 
-	server := api.NewApiServer(":8080",db)
-   if error:= server.Run(); error!=nil{
-	log.Fatal(error)
-   }
+	initStorage(database)
+
+	server := api.NewApiServer(":"+os.Getenv("PORT"), database)
+	if err := server.Run(); err != nil {
+		log.Fatal(err)
+	}
 }
 
-func initStorage(db *sql.DB){
-	err:=db.Ping()
-	if err != nil{
+func initStorage(db *sql.DB) {
+	if err := db.Ping(); err != nil {
 		log.Fatal(err)
 	}
 	log.Println("Database connected successfully")
